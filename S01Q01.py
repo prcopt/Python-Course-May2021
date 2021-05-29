7e278617085688798c13930032ed14237b6caf4f
prcopt <prcopt.prc@gmail.com>
2021-05-29 15:10:41 +0530

    Update and rename S01Q01 to S01Q01.py

--- S01Q01.py
+++ S01Q01.py
@@ -2,9 +2,13 @@
 Modify the first “Hello World” program to prompt for user’s name 
 and then wish the user by saying Hello followed by his name
 
+
+
 Example :
 If user's name is Sam, then expected output is :
 Hello Sam !!
 """
+
+
 name = input("Enter User's Name: ")
 print("Hello",name,"!!")
